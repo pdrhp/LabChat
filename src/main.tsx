251ffb3f0e0842d13beb0545bfac888d6e849795
pdrhp <pedrohsp2004@gmail.com>
2024-06-02 20:56:00 -0300

    chore: remove stric mode to prevent double render

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
@@ -6,11 +5,9 @@ import { ThemeProvider } from "./providers/theme-provider.tsx";
 import Routes from "./router.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <App>
           <Routes />
         </App>
     </ThemeProvider>
-  </React.StrictMode>
 );
