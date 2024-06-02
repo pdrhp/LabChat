57b848d12f79de703c0edb497aff1935ae4719da
pdrhp <pedrohsp2004@gmail.com>
2024-06-02 20:57:09 -0300

    feat: configured socket connection and added new components in chat page

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,20 +1,23 @@
 import { UserSession } from "@/Interfaces/UserSession";
 import { httpGet, httpPost } from "@/api/Client";
+import { HubConnection, HubConnectionBuilder, LogLevel } from "@microsoft/signalr";
 import {
   ReactNode,
   createContext,
   useContext,
   useEffect,
-  useState
+  useState,
 } from "react";
 
 interface AuthContextType {
   userSession: UserSession | undefined;
+  socketConnection: HubConnection | undefined;
   login: (userData: {
     email: string;
     password: string;
   }) => Promise<UserSession>;
   logout: () => Promise<UserSession>;
+  connectSignalR: () => Promise<void>;
 }
 
 interface AuthProviderProps {
@@ -27,13 +30,16 @@ const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const [userSession, setUserSession] = useState<UserSession | undefined>(
     undefined
   );
+  const [socketConnection, setSocketConnection] = useState<
+    HubConnection | undefined
+  >(undefined);
 
   useEffect(() => {
     httpGet<UserSession>("auth/verifySession").then((response) => {
-        if(response.flag){
-            setUserSession(response.data)
-        }
-    })    
+      if (response.flag) {
+        setUserSession(response.data);
+      }
+    });
   }, []);
 
   const login = async (userData: { email: string; password: string }) => {
@@ -56,8 +62,40 @@ const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
     return response.data;
   };
 
+  const connectSignalR = async () => {
+    try {
+      const conn = new HubConnectionBuilder()
+        .withUrl("http://localhost:5210/chat")
+        .configureLogging(LogLevel.Information)
+        .build();
+
+      setSocketConnection(conn);
+
+      conn.on("Teste", (arg1, arg2) => {
+        console.log(arg1, arg2);
+      });
+
+      conn.on("TesteEspecifico", (arg1, arg2) => {
+        console.log(arg1, arg2);
+      });
+
+      conn.on("ReceiveIndividualMessage", (sender: string, message: string) => {
+        console.log(sender, message);
+      });
+
+      conn.on("ReceiveMessageFromServer", (admin: string, message: string) => {
+        console.log(admin, message);
+      });
+
+      await conn.start();
+      await conn.invoke("Test");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <AuthContext.Provider value={{ userSession, login, logout }}>
+    <AuthContext.Provider value={{ userSession,socketConnection, login, logout, connectSignalR }}>
       {children}
     </AuthContext.Provider>
   );
--- src/pages/private/main-pages/chat-page.tsx
+++ src/pages/private/main-pages/chat-page.tsx
@@ -1,11 +1,30 @@
-import MessageInput from "@/components/message-input"
+import MessageInput from "@/components/message-input";
+import { useAuth } from "@/context/AuthContext";
+import { useEffect } from "react";
 
 const ChatPage = () => {
+
+    const { connectSignalR, socketConnection } = useAuth();
+
+
+    useEffect(() => {
+        connectSignalR();
+
+
+    }, []);
+
+
   return (
     <main className="h-full w-full flex p-2">
         <aside className="w-[25%] h-full border rounded-l-lg flex flex-col">
             <div className="w-full h-[10%] border-b flex justify-center items-center">
                 <h1 className="scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-4xl">Chat</h1>
+            </div>
+            <div className="flex-1 w-full flex flex-col">
+                <div className="h-[15%] w-full flex flex-col justify-center items-center">
+                    <p>Você não nenhuma conversa :(</p>
+                    <p className="font-bold cursor-pointer hover:text-blue-200">+ Adicionar conversa</p>
+                </div>
             </div>
         </aside>
         <div className="border border-l-0 w-full rounded-r-lg bg-zinc-900 flex flex-col p-1 gap-2">
