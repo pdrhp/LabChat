9df3d29d905b81986da24bddff055b0177c15e3f
pdrhp <pedrohsp2004@gmail.com>
2024-06-08 14:38:28 -0300

    refactor: componentizing chat area

---
+++ src/components/chat-container.tsx
@@ -0,0 +1,14 @@
+import MessageInput from "./message-input"
+
+const ChatContainer = () => {
+  return (
+    <>
+    <div className="h-[78%]"></div>
+        <div className=" flex-1">
+          <MessageInput />
+        </div>
+    </>
+  )
+}
+
+export default ChatContainer
