a82f86ef12f470b6317bd935b560b50224255d09
pdrhp <pedrohsp2004@gmail.com>
2024-06-22 10:29:51 -0300

    feat: added tan stack query lib to control the state of requisitions

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ReactDOM from "react-dom/client";
 import { Toaster } from "sonner";
 import App from "./App.tsx";
@@ -5,11 +6,16 @@ import "./index.css";
 import { ThemeProvider } from "./providers/theme-provider.tsx";
 import Routes from "./router.tsx";
 
+
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <App>
-          <Routes />
-        </App>
-        <Toaster richColors/>
-    </ThemeProvider>
+  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <QueryClientProvider client={queryClient}>
+      <App>
+        <Routes />
+      </App>
+      <Toaster richColors />
+    </QueryClientProvider>
+  </ThemeProvider>
 );
