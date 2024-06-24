8465b9b33570a1d39c436fe851b1afc77cec3658
pdrhp <pedrohsp2004@gmail.com>
2024-06-23 21:26:37 -0300

    fix: adding the missing properties in chatMessage interface

--- src/Interfaces/chat-message.ts
+++ src/Interfaces/chat-message.ts
@@ -4,7 +4,9 @@ interface ChatMessage {
     senderId: string;
     receiverId: string;
     message: string;
+    chatRequestId: number;
     timestamp: string;
+    confirmation: boolean;
 }
 
 export default ChatMessage;
